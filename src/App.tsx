6c0da68990eaa8a8b1d7dd9b322fa3f80c95b972
zsolt270 <zsolt270000@gmail.com>
2024-08-15 19:15:46 +0200

    finished main char and it's animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import MainChar from "./components/MainChar";
 
 function App() {
+	const [charStatus, setCharStatus] = useState<"idle" | "run" | "dead">("dead");
+
 	return (
 		<>
 			<MainChar />
--- src/components/MainChar.tsx
+++ src/components/MainChar.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import style from "./css.modules/mainChar.module.css";
 
 export default function MainChar() {
-	const [charStatus, setCharStatus] = useState<"idle" | "run" | "dead">("idle");
+	const [charStatus, setCharStatus] = useState<"idle" | "run" | "dead">("dead");
 	// ha statusz dead akkor returnolje a divvet a mainchar dead anime classal
 	//ha tatusz meg nem kezdődött a game akkor az idle anime classal
 	//ha elkezdődött akkor a run classal és ugyan ezek szerint a zombikat is
