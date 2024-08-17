a736a469206ddc5fe2f6f33da1d8b45471986ecf
zsolt270 <zsolt270000@gmail.com>
2024-08-17 19:26:19 +0200

    adding modal

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,16 +1,20 @@
+import style from "./css.modules/modal.module.css";
+
 export default function Modal() {
 	return (
 		<div
 			className='modal show d-block'
 			data-bs-backdrop='static'
 		>
-			<div className='modal-dialog'>
-				<div className='modal-content'>
-					<div className='modal-header justify-content-center'>
+			<div
+				className={`modal-dialog modal-dialog-centered text-white ${style.customModalDialog}`}
+			>
+				<div className={` modal-content ${style.customModalBG}`}>
+					<div className='text-center'>
 						<h1 className='modal-title fs-2'>About the game</h1>
 					</div>
 					<div className='modal-body'>The game is </div>
-					<div className='modal-footer d-flex justify-content-center'>
+					<div className='d-flex justify-content-center'>
 						<button
 							type='button'
 							className=''
