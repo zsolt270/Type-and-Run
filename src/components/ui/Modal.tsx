fb945f7ab4fb368dba16d5aee8ce3e10812a638a
zsolt270 <zsolt270000@gmail.com>
2025-03-19 20:26:00 +0100

    Add small text and comment to the modal. But the modal is on pause till the game mechanism itself is finished

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -4,7 +4,14 @@ export default function Modal({ setGameStatus }: { setGameStatus: (isPlaying: bo
   return (
     <div className={style.customModalBG}>
       <div className={style.customModalDialog}>
-        <h1>valami</h1>
+        <div className='row text-center'>
+          <h2 className=''>Type and Run</h2>
+        </div>
+        <div className='row mt-3'>
+          <p>This is a typer game about running from the danger of a zombie appocalyps.</p> <br />
+          <p>The goal of the game is to</p>
+          {/* Ez a modal lehetne inkább egy lapozható, ami leirja magát a játékot, a célját és lehetne talán belerakni egy-két képet a példaként */}
+        </div>
       </div>
     </div>
   );
