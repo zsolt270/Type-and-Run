c13e0b6294d4b7171cabb583d7f269a3494428a5
zsolt270 <zsolt270000@gmail.com>
2025-03-31 18:33:56 +0200

    example 6

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,7 @@
 //TODO btn for the bg_music turn on/off
 //példának commentek
 //még több példa2
-//újabb
+//újabb2
 export default function Button() {
   return <div className='btn bg-secondary'>Button</div>;
 }
